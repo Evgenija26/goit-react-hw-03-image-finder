593943e7f6bad652c811ff21e6d727f19e5997aa
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2022-12-16 11:23:41 +0200

    hw commit1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,12 @@
 import { GlobalStyle } from './GlobalStyle';
 
+
 export const App = () => {
-  <GlobalStyle />
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  <div>
+  <h1>Eugenia</h1>
+  <GlobalStyle /> 
+  </div>
+  )
 };
